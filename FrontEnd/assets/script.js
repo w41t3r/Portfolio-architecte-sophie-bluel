196cee090b9a31f48f9e9f10ff98d1b1f47a92f0
sebbai <bailly93@gmail.com>
2023-06-19 17:33:52 +0200

    [ADD] Filter buttons

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,23 +1,53 @@
 worksUrl = "http://127.0.0.1:5678/api/works"
 
+
+
 fetch(worksUrl)
     .then(response => response.json())
-    .then(worksArray => {
-        worksArrayLength = worksArray.length;
-        for (let i = 0; i < worksArrayLength; i++) {
-            console.log(worksArray[i].title);
-            let tagFigure = document.createElement('figure');
-            document.querySelector('.gallery').appendChild(tagFigure);
-
-            const galleryFigureArray = document.querySelectorAll('div.gallery figure');
-            let imgTag = document.createElement('img');
-            imgTag.src = worksArray[i].imageUrl;
-            imgTag.alt = worksArray[i].title;
-            galleryFigureArray[i].appendChild(imgTag);
-
-            let figcaptionTag = document.createElement('figcaption');
-            figcaptionTag.innerHTML = worksArray[i].title;
-            galleryFigureArray[i].appendChild(figcaptionTag);
-        }
+    .then(response => {
+        allWorksData = response;
+        displayWorks(allWorksData);
     });
 
+function displayWorks(worksData) {
+    worksDataLength = worksData.length;
+    for (i = 0; i < worksDataLength; i++) {
+        console.log("DISPLAY WORKS CAT = : " + worksData[i].category.name + "");
+    }
+    for (let i = 0; i < worksDataLength; i++) {
+        let tagFigure = document.createElement('figure');
+        document.querySelector('.gallery').appendChild(tagFigure);
+
+        const galleryFigureArray = document.querySelectorAll('div.gallery figure');
+        let imgTag = document.createElement('img');
+        imgTag.src = worksData[i].imageUrl;
+        imgTag.alt = worksData[i].title;
+        galleryFigureArray[i].appendChild(imgTag);
+
+        let figcaptionTag = document.createElement('figcaption');
+        figcaptionTag.innerHTML = worksData[i].title;
+        galleryFigureArray[i].appendChild(figcaptionTag);
+    }
+}
+
+
+const buttonsArray = document.querySelectorAll('.filter__btn');
+const buttonsArrayLength = buttonsArray.length;
+console.log("filter btn count : " + buttonsArray.length + "");
+
+initializeFilterBtn(1);
+
+function initializeFilterBtn(btnNumber) {
+    buttonsArray[btnNumber - 1].classList.add('filter__btn--selected');
+}
+
+for (let i = 0; i < buttonsArrayLength; i++) {
+    buttonsArray[i].addEventListener('click', function () {
+        for (let i = 0; i < buttonsArrayLength; i++) {
+            if (buttonsArray[i].className === 'filter__btn filter__btn--selected') {
+                buttonsArray[i].classList.remove('filter__btn--selected');
+            }
+        }
+        buttonsArray[i].classList.toggle('filter__btn--selected');
+    });
+}
