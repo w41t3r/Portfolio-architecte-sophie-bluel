0e752a441aa33b062170ae613584795384b3428f
sebbai <bailly93@gmail.com>
2023-07-25 15:47:25 +0200

    [UPDATE] Fix add new item

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -1,7 +1,7 @@
 import { generateTag } from "./functions.js"
 import { loggedUser } from "./functions.js"
 
-const worksUrl = "http://127.0.0.1:5678/api/works";
+const worksUrl = "http://127.0.0.1:5678/api/works/";
 const token = sessionStorage.getItem('token');
 let allWorksData;
 
@@ -63,7 +63,7 @@ function createModal() {
         generateTag("body .modal__header", "p", "header__update__text", "Mode édition", 0, 0, 0, 0);
         generateTag("body .modal__header ", "button", "header__update__button", "publier les changements", 0, 0, 0, 0);
 
-        
+
         let modal = document.createElement('div');
         modal.id = 'modal';
         modal.className = 'modal';
@@ -82,7 +82,7 @@ function fillModale(modal) {
         generateTag("body .modal__content__delete", "h3", "modal__title", "Galerie photo", 0, 0, 0, 0);
         generateTag("body .modal__content__delete", "div", "modal__selector", 0, 0, 0, 0, 0);
 
-        displayAllWorksInModal(allWorksData);
+        displayWorksInModal(allWorksData);
         generateTag("body .modal__content__delete", "div", "modal__line", 0, 0, 0, 0, 0);
         generateTag("body .modal__content__delete", "button", "button modal__add__btn", "Ajouter une photo", 0, 0, 0, 0);
         generateTag("body .modal__content__delete", "button", "modal__delete__btn", "Supprimer la galerie", 0, 0, 0, 0);
@@ -95,7 +95,6 @@ function fillModale(modal) {
         }
         document.querySelector('.modal__close__logo').addEventListener('click', closeModal);
         document.querySelector('.modal__add__btn').addEventListener('click', displayAddModal);
-        //        document.querySelector('.modal__add__btn').addEventListener('click', displayAddItem);
         document.querySelector('.modal__delete__btn').addEventListener('click', deleteItem);
     }
     if (modal === "ADD ITEM") {
@@ -106,19 +105,12 @@ function fillModale(modal) {
 
         //AJOUT FORM
         generateTag("body .modal__content__add", "form", "modal__form", 0, 0, 0, 0, 0);
-        /////////////////
-
         generateTag("body .modal__form", "div", "modal__img__container", 0, 0, 0, 0, 0);
         generateTag("body .modal__img__container", "i", "fa-solid fa-mountain-sun fa-flip-horizontal modal__img__logo", 0, 0, 0, 0, 0);
         generateTag("body .modal__img__container", "label", "button modal__img__add__label", "+ Ajouter photo", 0, 0, 0, "file__upload");
-
         generateTag("body .modal__img__container", "input", 0, 0, "file__upload", 0, "file", 0);
         generateTag("body .modal__img__container", "img", "img__hidden", 0, "img__upload", 0, 0, 0);
-
-
         generateTag("body .modal__img__container", "p", "modal__img__txt", "jpg.png : 4mo max", 0, 0, 0, 0);
-
-
         generateTag("body .modal__form", "label", "modal__title__work", "Titre", 0, 0, 0, "modal__title__input");
         generateTag("body .modal__form", "input", 0, 0, "modal__title__input", 0, 0, 0);
         generateTag("body .modal__form", "label", "modal__category__work", "Catégorie", 0, 0, 0, "modal__category__select");
@@ -143,15 +135,9 @@ function fillModale(modal) {
                 reader.readAsDataURL(that.files[0]);
                 let uploadBtn = document.getElementById('upload__btn');
                 uploadBtn.classList.add('green');
-                ///////////////////////////////////////////////////////
             }
         });
-
-        //       if (document.getElementById('upload__btn').className === 'button modal__upload__btn green') {
         document.getElementById('upload__btn').addEventListener('click', uploadItem);
-
-        //        }
-
         document.querySelector('.modal__arrow__logo').addEventListener('click', displayDeleteModal);
         document.querySelector('.modal__add__close__logo').addEventListener('click', closeModal);
     }
@@ -161,45 +147,15 @@ function fillModale(modal) {
 
 function uploadItem(e) {
     e.preventDefault();
-    /*    console.log(`E.classname = ${e.target.className}`)
-    //    if (e.target.className === 'button modal__upload__btn green') {
-    /*    let form = document.querySelector('form');
-        let fd = new FormData(form);
-    
-        let img = document.getElementById('img__upload').src;
-        let title = document.getElementById('modal__title__input').value;
-    */
-
-    /*    console.log(`CATEGORY = ${category}`);
-    let formElement = document.querySelector('form');
-    let formData = new FormData(formElement);
-    let request = new XMLHttpRequest();
-    request.open('POST', worksUrl);
-    request.send(formData);
-*/
-    let category = document.getElementById('modal__category__select').value;
-
     const formData = new FormData();
-    //    formData.append("title", document.getElementById('modal__title__input').value);
-    formData.title = document.getElementById('modal__title__input').value;
+    const title = document.getElementById('modal__title__input').value;
+    const file = document.getElementById('file__upload').files[0];
+    const category = document.getElementById('modal__category__select').value;
 
-    //    formData.append("imageUrl", document.getElementById('file__upload').src);
-
-    //    formData.append("imageUrl", document.getElementById('file__upload').files[0]);//
-    formData.imageUrl = document.getElementById('file__upload').files[0];
-
-    //    formData.append("categoryId", category.toString());
-    formData.categoryId = category.toString();
-
-    console.log(`FORMDATA img = ${formData.imageUrl}`);
-    /*    formData.append("userId", 1);
-        formData.append("id", 15);
-            formData.userId = 1;
-    formData.id = 15;
-        
-    
-    
-        //    let request = new Request(worksUrl, {*/
+    formData.append("title", title);
+    formData.append("image", file);
+    formData.append("category", category);
+    console.log(`FORMDATA img = ${formData.image}`);
 
     fetch(worksUrl, {
         method: 'POST',
@@ -211,42 +167,31 @@ function uploadItem(e) {
         body: formData,
     })
         .then((response) => {
-            if (!response.ok) throw new Error("Request failed. Code error: " + response.status);
+            if (response.status == 201) alert("Projet ajouté avec succès.");
+            else if (!response.ok) {
+                alert("/!\\ Erreur lors de l'ajout /!\\");
+                throw new Error("Request failed. Code error: " + response.status);
+            }
         })
         .catch((error) => {
             console.error("ERROR TO UPLOAD FILE", error);
         });
-
-
-    //    fetch(request)
-    //        .then((res) => res.json)
-    //        .then((data => {}))
-    //}
-    //    else return;
-}
-
-function displayAddItem(e) {
-    e.preventDefault;
-    //    document.querySelector('.modal__add__btn').removeEventListener('click', displayAddItem);
 }
 
 function deleteItem(e) {
     e.preventDefault();
-    //    const token = sessionStorage.getItem('token');
     console.log(`FUNCTION DELETE ITEM`);
-    //    const itemsContainer = document.querySelectorAll('.modal__item__container');
     const itemsToDelete = document.querySelectorAll('.trash__logo--selected');
     for (let i = 0; i < itemsToDelete.length; i++) {
         let elementIdTmp = itemsToDelete[i].getAttribute('id');
         let elementId = parseInt(elementIdTmp, 10);
-        //        itemsContainer[elementId - 1].remove();
         console.log(`Element : ${elementId} deleted`);
         fetchDelete(elementId);
     }
 }
 
 function fetchDelete(elementId) {
-    fetch(worksUrl + '/' + elementId, {
+    fetch(worksUrl + elementId, {
         method: 'DELETE',
         headers: {
             'Accept': 'application/json',
@@ -254,7 +199,11 @@ function fetchDelete(elementId) {
         },
     })
         .then((response) => {
-            if (!response.ok) throw new Error("Request failed. Code error: " + response.status);
+            if (response.status == 200) alert("Un ou plusieurs projet(s) supprimé(s) avec succès.");
+            else if (!response.ok) {
+                alert("/!\\ Erreur lors de la suppression /!\\");
+                throw new Error("Request failed. Code error: " + response.status);
+            }
         })
         .catch((error) => {
             console.error("ERROR TO DELETE ITEM", error);
@@ -262,7 +211,6 @@ function fetchDelete(elementId) {
 }
 
 function displayAddModal() {
-    console.log(`DISPLAY ADD`);
     document.querySelector('.modal__add__btn').removeEventListener('click', displayAddModal);
     document.querySelector('.modal').remove();
     createModal();
@@ -270,7 +218,6 @@ function displayAddModal() {
 }
 
 function displayDeleteModal() {
-    console.log(`DISPLAY DELETE`);
     document.querySelector('.modal__arrow__logo').removeEventListener('click', displayDeleteModal);
     document.querySelector('.modal').remove();
     createModal();
@@ -278,8 +225,6 @@ function displayDeleteModal() {
 }
 
 function closeModal(e) {
-    console.log(`CLOSE MODAL`);
-    console.log(`E CLASS NAME = ${e.target.className}`);
     if (document.getElementById('modal') === null) return;
     else {
         if (e.target.className === 'fa-solid fa-xmark modal__close__logo') {
@@ -307,14 +252,9 @@ function closeModal(e) {
     }
 }
 
-function displayAllWorksInModal(allWorksData) {
-
-    /*    if (document.querySelector('.modal__item__container') !== null) {
-            document.querySelector('.modal__item__container').remove();
-        }
-    */
-    for (i = 0; i < allWorksData.length; i++) {
-        let elementId = allWorksData[i].id;
+function displayWorksInModal(worksData) {
+    for (i = 0; i < worksData.length; i++) {
+        let elementId = worksData[i].id;
         generateTag("body .modal__selector", "div", "modal__item__container", 0, elementId, 0, 0, 0);
         let itemsContainer = document.querySelectorAll('.modal__item__container');
 
@@ -332,7 +272,7 @@ function displayAllWorksInModal(allWorksData) {
 
         generateTag("body .modal__item__container", "img", "modal__item", 0, 0, 0, 0, 0);
         let imageTag = document.querySelector('.modal__item__container > img:last-child');
-        imageTag.src = allWorksData[i].imageUrl;
+        imageTag.src = worksData[i].imageUrl;
         imageTag.id = "item__" + elementId;
         itemsContainer[i].appendChild(imageTag);
 
